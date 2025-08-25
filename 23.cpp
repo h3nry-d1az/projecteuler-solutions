dcf30efe0417a2bbc770dd3167e3bd520217b6d4
Henry Díaz Bordón <henrydiazbordon@gmail.com>
2025-08-25 23:26:33 +0100

    add solution to problem 23

---
+++ 23.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <cstdint>
+using namespace std;
+
+#include <set>
+
+#define ll long long
+
+vector<int> __memo_sigma(INT16_MAX);
+int sigma(int n)
+{
+    if (int m = __memo_sigma[n]; m) return m;
+    if (n <= 1) return 0;
+
+    int sum = 0;
+    for (int i = 1; i < n; i++) if (n % i == 0) sum += i;
+    __memo_sigma[n] = sum;
+    return sum;
+}
+
+int main()
+{
+    // ios::sync_with_stdio(false);
+    // cin.tie(nullptr);
+
+    set<int> abundant;
+    for (int n = 2; n <= 28123; n++) if (sigma(n) > n) abundant.insert(n);
+
+    set<int> valid;
+    ll sum = 0;
+    for (int n = 1; n <= 28123; n++)
+    {
+        bool invalid = false;
+        for (auto e = abundant.begin(); e != abundant.end(); e++)
+        {
+            if (abundant.find(n - *e) != abundant.end())
+            {
+                invalid = true;
+                break;
+            }
+        }
+        if (!invalid) {sum += n; valid.insert(n);}
+    }
+
+    // for (auto e = abundant.begin(); e != abundant.end(); e++) cout << *e << " ";
+    // cout << endl;
+    // for (auto e = valid.begin(); e != valid.end(); e++) cout << *e << " ";
+
+    cout << sum << endl;
+
+    return 0;
+}
