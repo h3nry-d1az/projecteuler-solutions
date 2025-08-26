a601c44e92179e30a45a100f2331fa4dfb3c2065
Henry Díaz Bordón <henrydiazbordon@gmail.com>
2025-08-26 15:31:10 +0100

    add documentation to problem 59

--- 59.py
+++ 59.py
@@ -1,15 +1,20 @@
 from functools import reduce
 
 decipher = lambda txt, key: map(lambda p: ord(p[0])^ord(p[1]), zip(txt, key*485))
+decipher.__doc__ = """Apply XOR decryption to the problem text with a given 3-letter password."""
 
 def points(c: str) -> int:
+    """Returns how plausible it is for `c` to be part of an English word."""
     if c in range(ord('a'), ord('z')+1): return 2
     if c in range(ord('A'), ord('Z')+1): return 1
     if chr(c) in {'(', ')', ' ', '\'', '"', '?', '!', '-', ',', '.'}: return 0
     return -5
+
 validate = lambda txt: sum(points(c) for c in txt)
+validate.__doc__ = """Apply `points` to the entire text and sum all points."""
 
 def main(encrypted: str) -> int:
+    """Solution entry point."""
     scores = []
     for a in (chr(i) for i in range(ord('a'), ord('z')+1)):
         for b in (chr(i) for i in range(ord('a'), ord('z')+1)):
